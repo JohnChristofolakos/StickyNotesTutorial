fa51e2c083cdbe47b8cb67746616274b36cfbfc9
John Christofolakos <john.christofolakos@gmail.com>
2018-02-19 16:24:11 -0500

    step 12 - post API

--- app.js
+++ app.js
@@ -5,11 +5,27 @@ var app = express();
 
 app.use(express.static("public"));
 
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.raw( { type: function() { return true; } } ));
+
 app.get("/api/notes", function(req, res) {
     res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
 
     var notes = fs.readJsonSync(path.resolve(__dirname, "notes.json"));
     res.json(notes);
+});
+
+app.post("/api/notes", function(req, res) {
+    var payload;
+    if (req.body instanceof Uint8Array) {
+        payload = JSON.parse(req.body.toString("utf8"));
+    }
+    else if (req.body instanceof Object) {
+        payload = req.body;
+    }
+    fs.writeJsonSync(path.resolve(__dirname, "notes.json"), payload);
+    res.status(200).send();
 });
 
 var server = app.listen(3000);
