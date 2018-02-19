684b63aafeb647aff69c501884e70a154cece711
John Christofolakos <john.christofolakos@gmail.com>
2018-02-19 16:20:57 -0500

    step 10 - get API

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
+var fs = require("fs-extra");
+var path = require("path");
 var express = require("express");
 var app = express();
 
 app.use(express.static("public"));
+
+app.get("/api/notes", function(req, res) {
+    res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+
+    var notes = fs.readJsonSync(path.resolve(__dirname, "notes.json"));
+    res.json(notes);
+});
+
 var server = app.listen(3000);
