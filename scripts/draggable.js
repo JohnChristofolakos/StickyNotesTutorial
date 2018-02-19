4b58fe4408b459261f163cec43c33b6bd59f2999
John Christofolakos <john.christofolakos@gmail.com>
2018-02-19 15:31:29 -0500

    step 8 - fix select

--- scripts/draggable.js
+++ scripts/draggable.js
@@ -1,49 +1,60 @@
-function makeDraggable(elem) {
-    var currX, currY;
-
-    elem.onmousedown = startDragging;
-
-    function startDragging(e) {
-        e = e || window.event;
-
-        // get the current mouse cursor position
-        currX = e.clientX;
-        currY = e.clientY;
-
-        // set handlers for mouse move, up
-        document.onmousemove = continueDragging;
-        document.onmouseup = endDragging;
+var draggable = (function() {
+    function makeUndraggable(elem) {
+        elem.onmousedown = null;
     }
 
-    function continueDragging(e) {
-        e = e || window.event;
+    function makeDraggable(elem) {
+        var currX, currY;
 
-        // calculate the mouse movement
-        offsetX = e.clientX - currX;
-        offsetY = e.clientY - currY;
-        currX = e.clientX;
-        currY = e.clientY;
+        elem.onmousedown = startDragging;
 
-        // set the element's new position:
-        elem.style.top = (elem.offsetTop + offsetY) + "px";
-        elem.style.left = (elem.offsetLeft + offsetX) + "px";
+        function startDragging(e) {
+            e = e || window.event;
+
+            // get the current mouse cursor position
+            currX = e.clientX;
+            currY = e.clientY;
+
+            // set handlers for mouse move, up
+            document.onmousemove = continueDragging;
+            document.onmouseup = endDragging;
+        }
+
+        function continueDragging(e) {
+            e = e || window.event;
+
+            // calculate the mouse movement
+            offsetX = e.clientX - currX;
+            offsetY = e.clientY - currY;
+            currX = e.clientX;
+            currY = e.clientY;
+
+            // set the element's new position:
+            elem.style.top = (elem.offsetTop + offsetY) + "px";
+            elem.style.left = (elem.offsetLeft + offsetX) + "px";
+        }
+
+        function endDragging(e) {
+            e = e || window.event;     
+
+            // clear the handlers when the mouse button is released
+            document.onmousemove = null;
+            document.onmouseup = null;
+        }
     }
 
-    function endDragging(e) {
-        e = e || window.event;     
-
-        // clear the handlers when the mouse button is released
-        document.onmousemove = null;
-        document.onmouseup = null;
+    var prevOnload = window.onload;
+    window.onload = function() {
+        for (var elem of document.getElementsByClassName("note")) {
+            makeDraggable(elem);
+        }
+        if (prevOnload) {
+            prevOnload.apply(this, arguments);
+        }
     }
-}
 
-var prevOnload = window.onload;
-window.onload = function() {
-    for (var elem of document.getElementsByClassName("note")) {
-        makeDraggable(elem);
-    }
-    if (prevOnload) {
-        prevOnload.apply(this, arguments);
-    }
-}
+    return {
+        makeDraggable: makeDraggable,
+        makeUndraggable: makeUndraggable
+    };
+} () )
--- scripts/editable.js
+++ scripts/editable.js
@@ -1,70 +1,78 @@
-function makeEditable(note) {
-    var actionBar = note.getElementsByClassName("action")[0];
+(function() {
+    function makeEditable(note) {
+        var actionBar = note.getElementsByClassName("action")[0];
 
-    var editButton = document.createElement("img");
-    editButton.src = "images/ic_edit_black_24px.svg";
-    editButton.className = "edit button";
-    editButton.onclick = startEdit;
-    actionBar.appendChild(editButton);
+        var editButton = document.createElement("img");
+        editButton.src = "images/ic_edit_black_24px.svg";
+        editButton.className = "edit button";
+        editButton.onclick = startEdit;
+        actionBar.appendChild(editButton);
 
-    var cancelButton = document.createElement("img");
-    cancelButton.src = "images/ic_clear_black_24px.svg";
-    cancelButton.className = "cancel button button_hidden";
-    cancelButton.onclick = cancelEdit;
-    actionBar.appendChild(cancelButton);
+        var cancelButton = document.createElement("img");
+        cancelButton.src = "images/ic_clear_black_24px.svg";
+        cancelButton.className = "cancel button button_hidden";
+        cancelButton.onclick = cancelEdit;
+        actionBar.appendChild(cancelButton);
 
-    var doneButton = document.createElement("img");
-    doneButton.src = "images/ic_done_black_24px.svg";
-    doneButton.className = "done button button_hidden";
-    doneButton.onclick = saveEdit;
-    actionBar.appendChild(doneButton);
-}
-
-var savedText;
-
-function startEdit() {
-    var actionBar = this.parentElement;
-    var note = actionBar.parentElement;
-
-    // make the textarea editable
-    note.querySelector("textarea").readOnly = false;
-
-    // update button visibility
-    actionBar.querySelector(".edit").classList.add("button_hidden");
-    actionBar.querySelector(".done").classList.remove("button_hidden");
-    actionBar.querySelector(".cancel").classList.remove("button_hidden");
-    
-    // save the current text
-    savedText = note.querySelector("textarea").value;
-}
-
-function saveEdit() {
-    var actionBar = this.parentElement;
-    var note = actionBar.parentElement;
-
-    // update button visibility
-    actionBar.querySelector(".edit").classList.remove("button_hidden");
-    actionBar.querySelector(".done").classList.add("button_hidden");
-    actionBar.querySelector(".cancel").classList.add("button_hidden");
-
-    // make the textarea uneditable
-    note.querySelector("textarea").readOnly = true;
-}
-
-function cancelEdit() {
-    var actionBar = this.parentElement;
-    var note = actionBar.parentElement;
-    note.querySelector("textarea").value = savedText;
-
-    actionBar.querySelector(".done").onclick();
-}
-
-var prevOnload = window.onload;
-window.onload = function() {
-    for (var note of document.getElementsByClassName("note")) {
-        makeEditable(note);
-    }
-    if (prevOnload) {
-        prevOnload.apply(this, arguments);
+        var doneButton = document.createElement("img");
+        doneButton.src = "images/ic_done_black_24px.svg";
+        doneButton.className = "done button button_hidden";
+        doneButton.onclick = saveEdit;
+        actionBar.appendChild(doneButton);
     }
-}
+
+    var savedText;
+
+    function startEdit() {
+        var actionBar = this.parentElement;
+        var note = actionBar.parentElement;
+
+        // make the textarea editable
+        note.querySelector("textarea").readOnly = false;
+
+        // update button visibility
+        actionBar.querySelector(".edit").classList.add("button_hidden");
+        actionBar.querySelector(".done").classList.remove("button_hidden");
+        actionBar.querySelector(".cancel").classList.remove("button_hidden");
+
+        // save the current text
+        savedText = note.querySelector("textarea").value;
+
+        // make it undraggable
+        draggable.makeUndraggable(note);
+    }
+
+    function saveEdit() {
+        var actionBar = this.parentElement;
+        var note = actionBar.parentElement;
+
+        // update button visibility
+        actionBar.querySelector(".edit").classList.remove("button_hidden");
+        actionBar.querySelector(".done").classList.add("button_hidden");
+        actionBar.querySelector(".cancel").classList.add("button_hidden");
+
+        // make the textarea uneditable
+        note.querySelector("textarea").readOnly = true;
+
+        // make it draggable again
+        draggable.makeDraggable(note);
+    }
+
+    function cancelEdit() {
+        var actionBar = this.parentElement;
+        var note = actionBar.parentElement;
+        note.querySelector("textarea").value = savedText;
+
+        actionBar.querySelector(".done").onclick();
+    }
+
+    var prevOnload = window.onload;
+    window.onload = function() {
+        for (var note of document.getElementsByClassName("note")) {
+            makeEditable(note);
+        }
+        if (prevOnload) {
+            prevOnload.apply(this, arguments);
+        }
+    }
+} () )
