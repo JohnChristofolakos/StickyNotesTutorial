b68f803e3c809f493dadc3b2a4ec149e38f63aac
John Christofolakos <john.christofolakos@gmail.com>
2018-02-19 15:14:20 -0500

    step 3 - closure

--- exercise1/doAlerts.js
+++
@@ -1,8 +0,0 @@
-function doAlerts(n) { 
-    for (var i = 0; i < n; i++ ) {
-        setTimeout( function() { alert("Alert number " + i); },
-            i * 100
-        );
-    };
-}
-doAlerts(3);
---
+++ scripts/draggable.js
@@ -0,0 +1,45 @@
+function makeDraggable(elem) {
+    var currX, currY;
+
+    elem.onmousedown = startDragging;
+
+    function startDragging(e) {
+        e = e || window.event;
+
+        // get the current mouse cursor position
+        currX = e.clientX;
+        currY = e.clientY;
+
+        // set handlers for mouse move, up
+        document.onmousemove = continueDragging;
+        document.onmouseup = endDragging;
+    }
+
+    function continueDragging(e) {
+        e = e || window.event;
+
+        // calculate the mouse movement
+        offsetX = e.clientX - currX;
+        offsetY = e.clientY - currY;
+        currX = e.clientX;
+        currY = e.clientY;
+
+        // set the element's new position:
+        elem.style.top = (elem.offsetTop + offsetY) + "px";
+        elem.style.left = (elem.offsetLeft + offsetX) + "px";
+    }
+
+    function endDragging(e) {
+        e = e || window.event;     
+
+        // clear the handlers when the mouse button is released
+        document.onmousemove = null;
+        document.onmouseup = null;
+    }
+}
+
+window.onload = function() {
+    for (var elem of document.getElementsByTagName("li")) {
+        makeDraggable(elem);
+    }
+}
