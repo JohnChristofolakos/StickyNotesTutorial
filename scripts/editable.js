a3c7e50dcacf1619aad23fdd01a49906f2f77410
John Christofolakos <john.christofolakos@gmail.com>
2018-02-19 15:22:19 -0500

    exercise 3

--- scripts/editable.js
+++ scripts/editable.js
@@ -6,9 +6,38 @@ function makeEditable(note) {
     editButton.className = "edit button";
     editButton.onclick = startEdit;
     actionBar.appendChild(editButton);
+
+    var cancelButton = document.createElement("img");
+    cancelButton.src = "images/ic_clear_black_24px.svg";
+    cancelButton.className = "cancel button button_hidden";
+    cancelButton.onclick = cancelEdit;
+    actionBar.appendChild(cancelButton);
+
+    var doneButton = document.createElement("img");
+    doneButton.src = "images/ic_done_black_24px.svg";
+    doneButton.className = "done button button_hidden";
+    doneButton.onclick = saveEdit;
+    actionBar.appendChild(doneButton);
 }
 
 function startEdit() {
+    var actionBar = this.parentElement;
+    var note = actionBar.parentElement;
+
+    // make the textarea editable
+    note.querySelector("textarea").readOnly = false;
+
+    // update button visibility
+    actionBar.querySelector(".edit").classList.add("button_hidden");
+    actionBar.querySelector(".done").classList.remove("button_hidden");
+    actionBar.querySelector(".cancel").classList.remove("button_hidden");
+}
+
+function saveEdit() {
+    // TODO
+}
+
+function cancelEdit() {
     // TODO
 }
 
