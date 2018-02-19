202e47022f7082fbf94efc191efdf8939f2df01a
John Christofolakos <john.christofolakos@gmail.com>
2018-02-19 16:28:18 -0500

    step 14 - load

--- public/scripts/persist.js
+++ public/scripts/persist.js
@@ -14,6 +14,33 @@ var persist = function() {
         xhr = new XMLHttpRequest();
         xhr.open("POST", "/api/notes");
         xhr.send( JSON.stringify(notes) );
+        }
+
+    function loadNotes() {
+        xhr = new XMLHttpRequest();
+        xhr.onload = loadRequestReady;
+        xhr.open("GET", "/api/notes");
+        xhr.send();
+    }
+    
+    function loadRequestReady() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                alert(xhr.responseText);            // TODO
+            }
+            else {
+                alert("Cannot load notes, status = " + xhr.status);
+            }
+        }
+    }
+
+    var prevOnload = window.onload;
+    window.onload = function() {
+        loadNotes();
+
+        if (prevOnload) {
+            prevOnload.apply(this, arguments);
+        }
     }
 
     return {
